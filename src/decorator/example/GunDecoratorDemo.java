e952e10397f22a4587741b2f0b65c93f309b0b96
hushi1991 <davi0279@stud.kea.dk>
2018-06-06 15:10:34 +0200

    herp

--- src/decorator/example/GunDecoratorDemo.java
+++ src/decorator/example/GunDecoratorDemo.java
@@ -3,6 +3,9 @@ package decorator.example;
 public class GunDecoratorDemo {
     public static void main(String[] args) {
         Gun gun = new ScopeDecorator(new SilencerDecorator(new BaseGun("9 mm")));
+        Gun gun1 = new BaseGun("5.56 mm");
+        gun1.fire();
+        System.out.println("Test gun1");
         gun.fire();
     }
 }
